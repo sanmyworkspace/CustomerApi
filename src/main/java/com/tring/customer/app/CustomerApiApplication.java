948134d9b10ff759dc6b203ba6401b35b8738181
aksankar <aksankar@MYKULWN5010437.kul-dc.dhl.com>
2018-07-25 11:47:23 +0800

    removed all the commented code

--- src/main/java/com/tring/customer/app/CustomerApiApplication.java
+++ src/main/java/com/tring/customer/app/CustomerApiApplication.java
@@ -14,54 +14,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @PropertySource(value = { "classpath:/log4j.properties"})
 public class CustomerApiApplication extends SpringBootServletInitializer {
-
+	
 	static Logger logger = LogManager.getLogger(CustomerApiApplication.class);
-
-	/*@Autowired
-	private Environment environment;
-
-	@Autowired
-	@Bean(name = "customerSessionFactory")
-	public LocalSessionFactoryBean getSessionFactory() {
-		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-		sessionFactoryBean.setDataSource(getDataSource());
-		sessionFactoryBean.setPackagesToScan(new String[] { "com.tring.customer.model" });
-		sessionFactoryBean.setHibernateProperties(getHibernateProperties());
-		return sessionFactoryBean;
-	}
-
-	private DataSource getDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(environment.getProperty("db.driverClassName"));
-		dataSource.setUrl(environment.getProperty("db.url"));
-		dataSource.setUsername(environment.getProperty("db.username"));
-		dataSource.setPassword(environment.getProperty("db.password"));
-		return dataSource;
-	}
-
-	private Properties getHibernateProperties() {
-		Properties properties = new Properties();
-		properties.put("hibernate.dialect", environment.getProperty("db.dialect"));
-		properties.put("hibernate.show_sql", environment.getProperty("db.show_sql"));
-		properties.put("hibernate.format_sql", environment.getProperty("db.format_sql"));
-		properties.put("hibernate.hbm2ddl.auto", environment.getProperty("db.hbm2ddl.auto"));
-		return properties;
-	}
-
-	@Autowired
-	@Bean(name = "customerHibernateTemplate")
-	public HibernateTemplate getHibernateTemplate() {
-		HibernateTemplate hibernateTemplate = new HibernateTemplate(getSessionFactory().getObject());
-		return hibernateTemplate;
-	}
-
-	@Autowired
-	@Bean(name = "customerTxManager")
-	public HibernateTransactionManager geTransactionManager() {
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(getSessionFactory().getObject());
-		return txManager;
-	}*/
 	
 	public static void main(String[] args) {
 		logger.debug("CustomerApiApplication is started intializing...........");
